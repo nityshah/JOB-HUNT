a4e3dfdfc1fefa251cff3f109fa093976dcdb43c
nitya3676 <nitya.shah3676@gmail.com>
2025-06-13 21:32:04 +0530

    cookie changes done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -109,7 +109,7 @@ export const login = async (req, res) => {
 
 
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None',secure:true }).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
